353dc749f934fc0f012318a755d4558a83e08e3a
walkerdorris <walker.dorris@gmail.com>
2015-09-30 22:27:16 -0500

    no errors, but not working

--- javascripts/get-comic-types.js
+++ javascripts/get-comic-types.js
@@ -3,7 +3,7 @@ define(function(require) {
   var Q = require("q");
   return {
     getComicsTypes: function() {
-      var deffered = Q.defer();
+      var deferred = Q.defer();
         $.ajax({url: "https://walker-comic-types.firebaseio.com/"})
           .done(function(json_data) {
             deferred.resolve(json_data);
